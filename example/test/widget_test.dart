a68f40208a01515f8a363b44e51a30834fd854b9
Kevin Johnson <kevinjohnsonutx@gmail.com>
2020-12-15 15:23:35 -0600

    revert

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:optimizely_plugin_example/main.dart';
 
 void main() {
@@ -17,8 +18,8 @@ void main() {
     // Verify that platform version is retrieved.
     expect(
       find.byWidgetPredicate(
-        (Widget widget) =>
-            widget is Text && widget.data.startsWith('Running on:'),
+        (Widget widget) => widget is Text &&
+                           widget.data.startsWith('Running on:'),
       ),
       findsOneWidget,
     );
