6afa19eb6f39445ff287b45c1f2e1c9a7546da4e
Kevin Johnson <kevinjohnsonutx@gmail.com>
2020-12-15 14:09:46 -0600

    move to arg of state

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,22 +5,10 @@ import 'package:flutter/services.dart';
 import 'package:optimizely_plugin/optimizely_plugin.dart';
 
 Future<void> main() async {
-  // Uses the Optimizely example:
-  // https://docs.developers.optimizely.com/full-stack/docs/example-datafile
-  final dataFile = await rootBundle.loadString('assets/datafile.json');
-  var optimizelyPlugin = OptimizelyPlugin();
-  await optimizelyPlugin.initOptimizelyManager(
-    'your_optimizely_sdk_key',
-    dataFile,
-  );
-  runApp(MyApp(optimizelyPlugin: optimizelyPlugin));
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({@required this.optimizelyPlugin});
-
-  final OptimizelyPlugin optimizelyPlugin;
-
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -28,10 +16,23 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _priceFilterFlag = 'unknown';
   String _minPriceVariable = 'unknown';
+  final OptimizelyPlugin optimizelyPlugin = OptimizelyPlugin();
 
   @override
   void initState() {
     super.initState();
+    setupOptimizely();
+  }
+
+  void setupOptimizely() async {
+    // Uses the Optimizely example:
+    // https://docs.developers.optimizely.com/full-stack/docs/example-datafile
+    final dataFile =
+        await DefaultAssetBundle.of(context).loadString('assets/datafile.json');
+    await optimizelyPlugin.initOptimizelyManager(
+      'your_optimizely_sdk_key',
+      dataFile,
+    );
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -41,7 +42,7 @@ class _MyAppState extends State<MyApp> {
     var platform =
         Theme.of(context).platform.toString().split('.')[1].toLowerCase();
     try {
-      bool featureEnabled = await widget.optimizelyPlugin.isFeatureEnabled(
+      bool featureEnabled = await optimizelyPlugin.isFeatureEnabled(
         'price_filter',
         'user@example.org',
         {'platform': platform},
@@ -67,7 +68,7 @@ class _MyAppState extends State<MyApp> {
     var platform =
         Theme.of(context).platform.toString().split('.')[1].toLowerCase();
     try {
-      variables = await widget.optimizelyPlugin.getAllFeatureVariables(
+      variables = await optimizelyPlugin.getAllFeatureVariables(
         'price_filter',
         'user@example.org',
         {'platform': platform},
